6aa7fb850aa7c916a9e8716b02109daf317bc13d
Ashish-9455187250 <94695243+Ashish-9455187250@users.noreply.github.com>
2023-07-05 16:21:58 +0530

    A stopwatch application built on Android studio in Java and XML

---
+++ app/src/main/java/com/example/appdev_practice/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.appdev_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        runTimer();
+    }
+
+    public void onClickStart(View view){
+        running = true;
+    }
+
+    public void onClickStop(View view){
+        running = false;
+    }
+
+    public void onClickReset(View view){
+        running = false;
+        seconds = 0;
+    }
+
+    public void runTimer(){
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds % 3600)/60;
+                int secs = seconds % 60;
+
+                String time = String.format(Locale.getDefault(),"   %d:%02d:%02d",hours,minutes,secs);
+                timeView.setText(time);
+                if (running){
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+    }
+
+
+
+
+}
